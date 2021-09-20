2cecaee1a4ae516aa34c035dc57c4b84d84ce2f1
Philippe Guégan <philippe.guegan@gmail.com>
2021-09-20 19:13:16 +0200

    Finalisation

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,7 @@ module Touiteur
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.action_view.field_error_proc = Proc.new { |html_tag, _| html_tag.html_safe }
   end
 end
